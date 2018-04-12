a5caf60048b0662b71e3f8e2a2de8309d2f83e37
liangshengzero <zczyyx0214@163.com>
2018-04-12 14:54:37 +0800

    Update credit_card_number.py

--- credit_card_number.py
+++ credit_card_number.py
@@ -46,5 +46,5 @@ def credit_card_number(prefixList, howMany=1, length=16):
     return result
 
 if __name__ == "__main__":
- fakecard = credit_card_number(['545719022'], 30,16)
+ fakecard = credit_card_number(['4388540057'], 30,16)
  print fakecard
